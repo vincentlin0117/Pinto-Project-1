71e0099b10ddafa55f5367d3d2a39020598198e4
vincent lin <vincentlin0117@gmail.com>
2023-09-11 13:40:04 -0700

    First commit

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -335,7 +335,7 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  thread_current ()->priority = new_priority;
+  return;
 }
 
 /* Returns the current thread's priority. */
